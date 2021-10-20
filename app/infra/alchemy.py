df6b4d0f5dd57c806a05f38ec34cbcf452910591
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-10-20 22:49:52 +0900

    :+1: Fix session manage

--- app/infra/alchemy.py
+++ app/infra/alchemy.py
@@ -1,4 +1,5 @@
 import configparser
+from contextlib import contextmanager
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -18,31 +19,49 @@ def get_env(key: str, default: str) -> str:
     """
     _parser = configparser.ConfigParser()
     _parser.read("./config.ini")
-    val = _parser[key]
+    val = _parser["DEFAULT"][key]
     return str(val) if val else default
 
 
-__DATABASE = get_env("engine", "mysql")
+__ENGINE = get_env("engine", "mysql")
 __USER = get_env("user", "agni")
-__PASSWORD = get_env("agni", "agni")
+__PASSWORD = get_env("password", "agni")
 __HOST = get_env("host", "0.0.0.0")
 __PORT = get_env("port", "3306")
 __DB_NAME = get_env("database", "agni")
 
-__DATABASE_URL = f"{__DATABASE}://{__USER}:{__PASSWORD}@{__HOST}:{__PORT}/{__DB_NAME}"
+__DATABASE = "%s+mysqlconnector://%s:%s@%s:%s/%s?charset=utf8" % (
+    __ENGINE,
+    __USER,
+    __PASSWORD,
+    __HOST,
+    __PORT,
+    __DB_NAME,
+)
+
 
 Base = declarative_base()
 
 
-def get_session():
+@contextmanager
+def session_scope():
     """
     Get SQLAlchemy session object
     """
-    engine = create_engine(__DATABASE_URL)
-    SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+    engine = create_engine(__DATABASE, encoding="utf-8", echo=True)
+    Session = sessionmaker(
+        bind=engine,
+        autocommit=False,
+        # autoflush=False,
+        expire_on_commit=False,
+    )
+    session = Session()
 
-    session = SessionLocal()
     try:
         yield session
+        session.commit()
+    except Exception as exc:
+        session.rollback()
+        raise exc
     finally:
         session.close()
