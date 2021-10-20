66b213afb756a16594cf910a9efcac8cb5fc7369
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-10-20 23:14:57 +0900

    :card_file_box: Update init sql files

--- database/mysql/initdb.d/20210503152123_create_table_completion_code.sql
+++ database/mysql/initdb.d/20210503152123_create_table_completion_code.sql
@@ -1,11 +1,10 @@
--- migrate:up
 CREATE TABLE completion_codes (
     id INT(11) NOT NULL AUTO_INCREMENT,
-    uid VARCHAR(255) NOT NULL DEFAULT '',
+    user_id INT(11) NOT NULL,
     completion_code INT(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    KEY fk_completion_user_id (user_id),
+    CONSTRAINT fk_completion_user_id
+        FOREIGN KEY (user_id) REFERENCES users (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
-
--- migrate:down
-DROP TABLE IF EXISTS completion_codes;
---
+++ database/mysql/initdb.d/20211020060614_create_table_dwell_times.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS dwell_times (
+  user_id INT(11) NOT NULL,
+  task_id INT(11) NOT NULL,
+  condition_id INT(11) NOT NULL,
+  dwell_time INT(11) NOT NULL DEFAULT 0,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (user_id, task_id),
+  KEY fk_dwell_time_user_id (user_id),
+  CONSTRAINT fk_dwell_time_user_id
+    FOREIGN KEY (user_id) REFERENCES users (id),
+  KEY fk_dwell_time_task_id (task_id),
+  CONSTRAINT fk_dwell_time_task_id
+    FOREIGN KEY (task_id) REFERENCES tasks (id),
+  KEY fk_dwell_time_condition_id (condition_id),
+  CONSTRAINT fk_dwell_time_condition_id
+    FOREIGN KEY (condition_id) REFERENCES conditions (id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
